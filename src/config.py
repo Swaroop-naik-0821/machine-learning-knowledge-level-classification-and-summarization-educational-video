be6fb85113e6dcd1db72c489e1270eafaba9cf4f
Swaroop-naik-0821 <swaroopnaik0821@gmail.com>
2026-07-17 00:12:31 +0530

    Add configuration variables for paths and files

---
+++ src/config.py
@@ -0,0 +1,9 @@
+VIDEO_PATH = "input/video.mp4"
+
+TRANSCRIPT_JSON = "data/transcripts/clean_whisper.json"
+
+SENTENCE_JSON = "data/transcripts/sentence_timestamps.json"
+
+MODEL_PATH = "models/bert_knowledge_model"
+
+SUMMARY_OUTPUT = "outputs/led_summary.txt"
